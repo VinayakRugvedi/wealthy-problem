844f3b5cabc5c254a1fa78a393d7959c8e9b4b36
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-04-10 10:30:59 +0530

    Create Calendar component to hold and deal with calendar

---
+++ src/components/Calendar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import ReactCalendar from 'react-calendar'
+import RBC from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
+import '../styles/Calendar.scss'
+
+const localizer = RBC.momentLocalizer(moment)
+
+class Calendar extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      priceEvents : []
+    }
+  }
+
+  render () {
+    return (
+      <div className="calendarWrapper">
+        <RBC
+        localizer={localizer}
+        events={this.state.priceEvents}
+        startAccessor="start"
+        endAccessor="end"/>
+      </div>
+    )
+  }
+}
+
+export default Calendar
