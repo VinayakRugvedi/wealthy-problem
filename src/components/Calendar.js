a58cf2a5681f4d48473e3895cb9651f2399674fe
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-04-11 01:54:50 +0530

    Update state, React calendar and setup day component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,15 +3,42 @@ import React from 'react'
 import ReactCalendar from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import moment from 'moment'
+
+import Day from './Day'
 import '../styles/Calendar.scss'
 
-const localizer = RBC.momentLocalizer(moment)
+const localizer = ReactCalendar.momentLocalizer(moment)
 
 class Calendar extends React.Component {
   constructor () {
     super()
     this.state = {
-      priceEvents : []
+      priceEvents : [{
+      id: 0,
+      title: 'All Day Event very long title',
+      allDay: true,
+      start: 1,
+      end: 2,
+      }],
+      dayComponent: '',
+      dayObject: {}
+    }
+    this.setUpDayComponent = this.setUpDayComponent.bind(this)
+  }
+
+  setUpDayComponent (dayObject) {
+    return (
+      <Day label={dayObject.label} date={dayObject.date}
+        updateStockDetails={this.props.updateStockDetails}
+        stockDetails={this.props.stockDetails}
+        updateStockPrice={this.props.updateStockPrice}
+        removeStockPrice={this.props.removeStockPrice}/>
+    )
+  }
+
+  customDayPropGetter = (date) => {
+    return {
+      className: 'dayTile'
     }
   }
 
@@ -19,10 +46,20 @@ class Calendar extends React.Component {
     return (
       <div className="calendarWrapper">
         <ReactCalendar
-        localizer={localizer}
-        events={this.state.priceEvents}
-        startAccessor="start"
-        endAccessor="end"/>
+          localizer={localizer}
+          events={[]}
+          startAccessor="start"
+          endAccessor="end"
+          components={{
+            month: {
+              dateHeader: this.setUpDayComponent
+            }
+          }}
+          views={{
+            month: true // To have Month view only
+          }}
+          dayPropGetter={this.customDayPropGetter}
+        />
       </div>
     )
   }
